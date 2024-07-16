bb008db5a030a7488d048fe617f547dda0f8cb6b
Anushanga2001 <anushanga2001@gmail.com>
2024-07-16 23:12:42 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <iframe title="Test" width="1140" height="541.25" 
+      src="https://app.powerbi.com/reportEmbed?reportId=3f8f82e7-0ef0-4458-980f-7b547cc1a1ff&autoAuth=true&embeddedDemo=true" 
+      frameborder="0" allowFullScreen="true">
+      </iframe>
+    </div>
+  )
+}
